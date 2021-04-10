e3e8af1a7859f6b7f03929cfb166d7657523ad64
Jagadeesh Jayachandran <51157326+JagadeeshJayachandran@users.noreply.github.com>
2021-04-10 20:19:50 +0100

    Create functionReturnsFunction.js

---
+++ functionReturnsFunction.js
@@ -0,0 +1,16 @@
+//Function returns function
+
+// const whoKnows = str1 => str2 => `${str1}, ${str2} is related to IT`;
+// console.log(whoKnows(' Hi')('GCP'));
+
+// const counter = 0;
+// const add = str => () => `${str}`;
+
+// console.log(add('U'));
+
+// //call and apply
+// const meditation = {
+//   time: '10 mins',
+//   rule: 'Do not move',
+//   technique: 'Anapana',
+// };
