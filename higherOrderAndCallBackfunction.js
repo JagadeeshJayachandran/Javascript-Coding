33dad302ceb851dfb49650845da639fc14413db3
Jagadeesh Jayachandran <51157326+JagadeeshJayachandran@users.noreply.github.com>
2021-04-10 20:19:15 +0100

    Create higherOrderAndCallBackfunction.js

---
+++ higherOrderAndCallBackfunction.js
@@ -0,0 +1,13 @@
+// //higher order function and callback function
+// const transformer = function (oneWord, fn) {
+//   console.log(fn(oneWord));
+//   console.log(`Transformed by : ${fn.name}`);
+// };
+
+// transformer('j aga dee sh', firstWordUp); //call back function
+// transformer('j aga dee sh', oneWord); //Call back function
+// //output
+// J aga dee sh
+// script.js:53 Transformed by : firstWordUp
+// script.js:52 jagadeesh
+// script.js:53 Transformed by : oneWord
